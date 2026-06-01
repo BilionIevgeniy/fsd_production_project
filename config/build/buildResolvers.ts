84d2c11146a7f923a9b8d8414e14bc74fecd3a28
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-06-01 18:16:50 +0200

    added route

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,9 +1,12 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
-export const buildResolvers = (options: BuildOptions): webpack.ResolveOptions => {
+export const buildResolvers = (
+  options: BuildOptions,
+): webpack.ResolveOptions => {
   return {
     extensions: ['.tsx', '.ts', '.js'],
+    // modules - where to look for modules
     modules: [options.paths.src, 'node_modules'],
   };
 };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,24 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Navbar } from '../widgets/Navbar';
 import { Sidebar } from '../widgets/Sidebar';
-import { AppRouter } from './providers/router';
+import { AppRouter } from './providers/RouterProvider';
 import './styles/index.scss';
 import { Loader } from '../shared/ui';
+export enum Theme {
+  NORMAL = 'normal',
+  DARK = 'dark',
+}
 
 function App() {
+  const [theme, setTheme] = useState<Theme>(Theme.NORMAL);
+  const toggleTheme = () => {
+    setTheme(theme === Theme.NORMAL ? Theme.DARK : Theme.NORMAL);
+  };
   return (
     <Suspense fallback={<Loader />}>
-      <div>
+      <div className={`app ${theme}`}>
+        <button onClick={toggleTheme}>Switch Theme</button>
         <Navbar />
         <div className="content-page">
           <Sidebar children="Mksdfsdfd" />

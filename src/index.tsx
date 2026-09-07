0f99cde1f235f6e311cd9a48c339fbdc873ea336
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-09-07 18:48:59 +0200

    check test on CI

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
-import App from './app/App';
 import 'shared/config/language/i18n';
+import App from './app/App';
 
 render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>

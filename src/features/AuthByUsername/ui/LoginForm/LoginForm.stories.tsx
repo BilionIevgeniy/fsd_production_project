24f04835cb4d8c1e2a187730bd8bbca6e674c49f
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-09-22 13:12:55 +0200

    added storybook to LoginForm

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { StoreDecorator } from 'shared/config/storybook';
+import { StoreDecorator, TranslationDecorator } from 'shared/config/storybook';
 import { LoginForm } from './LoginForm';
 
 export default {
@@ -15,4 +15,12 @@ const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args
 
 export const Primary = Template.bind({});
 Primary.args = {};
-Primary.decorators = [StoreDecorator({})];
+Primary.decorators = [TranslationDecorator, StoreDecorator({})];
+
+export const WithError = Template.bind({});
+WithError.args = {};
+WithError.decorators = [TranslationDecorator, StoreDecorator({ loginForm: { error: 'error message' } })];
+
+export const Loading = Template.bind({});
+Loading.args = {};
+Loading.decorators = [TranslationDecorator, StoreDecorator({ loginForm: { isLoading: true } })];

45406a62ce95095706ea3ab99a495c1bf3365e1c
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-09-25 12:41:58 +0200

    fix login form stiry book

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,7 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { StoreDecorator, TranslationDecorator } from 'shared/config/storybook';
+import { ReducersList } from 'shared/lib/components/DynamicModuleLoader';
 import LoginForm from './LoginForm';
 import { loginReducer } from '../../model/slice/loginSlice';
+
+const defaultAsyncReducers: ReducersList = {
+  loginForm: loginReducer,
+};
 
 export default {
   title: 'features/AuthByUsername/LoginForm',
@@ -16,5 +21,5 @@ Primary.decorators = [StoreDecorator({})];
 
 export const WithError = Template.bind({});
 WithError.decorators = [
-  StoreDecorator({ loginForm: { error: 'error text' } }, { loginForm: loginReducer }),
+  StoreDecorator({ loginForm: { error: 'error text' } }, defaultAsyncReducers),
 ];
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -2,18 +2,12 @@ import { Story } from '@storybook/react';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import { DeepPartial, StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 import { ReducersList } from 'shared/lib/components/DynamicModuleLoader';
-import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
-
-const defaultAsyncReducers: ReducersList = {
-  loginForm: loginReducer,
-};
 
 // use for components that read the Redux store directly (useSelector/useDispatch)
-export const StoreDecorator = (
-  state: DeepPartial<StateSchema>,
-  asyncReducers?: ReducersList,
-) => (StoryComponent: Story) => (
-  <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
-    <StoryComponent />
-  </StoreProvider>
-);
+export const StoreDecorator =
+  (state: DeepPartial<StateSchema>, asyncReducers?: ReducersList) => (StoryComponent: Story) =>
+    (
+      <StoreProvider initialState={state} asyncReducers={asyncReducers}>
+        <StoryComponent />
+      </StoreProvider>
+    );
